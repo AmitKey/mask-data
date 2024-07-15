9aeda62b234962913a640e305bb74b8f5ab303d8
amit_k <htfgr2rr3yaf6ud3a46i5dn3esaycqeswzaur5ffl2jqrvia6m6a>
2024-07-15 09:35:30 +0300

    Update test

--- test.py
+++ test.py
@@ -1,18 +1,39 @@
-import unittest
+import pytest
 from utils.masking import mask_credit_card, mask_phone_number, mask_ip_address
 
 
-class TestMaskingFunctions(unittest.TestCase):
+def test_mask_credit_card():
+    # Arrange
+    input_text = "My credit card number is 1234567891011120"
+    expected_output = "My credit card number is ***"
 
-	def test_mask_credit_card(self):
-		self.assertEqual(mask_credit_card("My credit card number is 1234567891011120"), "My credit card number is ***")
+    # Act
+    result = mask_credit_card(input_text)
 
-	def test_mask_phone_number(self):
-		self.assertEqual(mask_phone_number("Call me at 123456789"), "Call me at ***")
-
-	def test_mask_email(self):
-		self.assertEqual(mask_ip_address("My IP is 192.180.1.1"), "My IP is ***")
+    # Assert
+    assert result == expected_output
 
 
-if __name__ == '__main__':
-	unittest.main()
+def test_mask_phone_number():
+    # Arrange
+    input_text = "Call me at 123456789"
+    expected_output = "Call me at ***"
+
+    # Act
+    result = mask_phone_number(input_text)
+
+    # Assert
+    assert result == expected_output
+
+
+def test_mask_ip_address():
+    # Arrange
+    input_text = "My IP is 192.180.1.1"
+    expected_output = "My IP is ***"
+
+    # Act
+    result = mask_ip_address(input_text)
+
+    # Assert
+    assert result == expected_output
+
