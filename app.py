de9107d33b68f4821f7917ee1e6d52e0c459a034
amit_k <htfgr2rr3yaf6ud3a46i5dn3esaycqeswzaur5ffl2jqrvia6m6a>
2024-07-15 13:55:01 +0300

    add logs

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
 from flask import Flask, request, jsonify
 from config import Config
 from services import mask_sensitive_info
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)  # update
+    app.config.from_object(Config)
+
+    # Configure logging to write to a file
+    log_file = 'app.log'
+    handler = RotatingFileHandler(log_file, maxBytes=10000, backupCount=1)
+    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.INFO)  # Set logging level to INFO
 
     @app.route('/sensitive-info-mask', methods=['POST'])
     def mask_text():
-        data = request.get_json()
-        if not data or 'text' not in data:
-            return jsonify({'error': 'No key called text provided'}), 400
+        try:
+            data = request.get_json()
+            original_text = data['text']
 
-        original_text = data['text']
-        masked_text = mask_sensitive_info(original_text)
-        return jsonify({'masked_text': masked_text})
+            # Log the incoming request
+            app.logger.info(f"Received request to mask text: {original_text}")
+
+            # Mask sensitive information
+            masked_text = mask_sensitive_info(original_text)
+
+            # Log the masked result
+            app.logger.info(f"Masked text: {masked_text}")
+
+            # Return JSON response with masked text and HTTP status 200 (OK)
+            return jsonify({'masked_text': masked_text}), 200
+
+        except Exception as e:
+            app.logger.error(f"Error processing request: {str(data)}")
+            # Return JSON response with error message and HTTP status 500 (Internal Server Error)
+            return jsonify({'error': 'A key named text is required'}), 500
 
     return app
 
